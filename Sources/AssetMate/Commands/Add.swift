f97e17c2dfe2257e4b1c975229d442d4765c4a64
Alexey Korolev <alphatroya@gmail.com>
2020-04-23 16:53:24 +0300

    Add add command

---
+++ Sources/AssetMate/Commands/Add.swift
@@ -0,0 +1,61 @@
+//
+// iOS Color Mate
+// Copyright © 2020 Alexey Korolev <alphatroya@gmail.com>
+//
+
+import ArgumentParser
+import Foundation
+
+struct ImageAsset: Encodable {
+    struct Image: Encodable {
+        let filename: String
+        let idiom: String
+    }
+
+    let images: [Image]
+    let info: ColorAsset.Info
+
+    init(imageName: String) {
+        images = [
+            .init(filename: imageName, idiom: "universal"),
+        ]
+        info = .init(version: 1, author: "xcode")
+    }
+}
+
+struct Add: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Add given .pdf file to the Asset folder"
+    )
+
+    @Argument(help: "Path to the pdf file")
+    var file: String
+
+    @Option(name: .shortAndLong, help: "Path to output Assets.xcassets folder")
+    var output: String
+
+    @Option(name: .shortAndLong, help: "Name for a image asset")
+    var name: String
+
+    func run() throws {
+        guard file.hasSuffix(".pdf") else {
+            throw ValidationError("That command support only pdf files")
+        }
+
+        let folder = URL(fileURLWithPath: output)
+            .appendingPathComponent(name)
+            .appendingPathExtension("imageset")
+        let manager = FileManager.default
+        guard !manager.fileExists(atPath: folder.path) else {
+            throw ValidationError("Folder \(name) did exist in Asset catalog")
+        }
+        try manager.createDirectory(at: folder, withIntermediateDirectories: true)
+        let pdf = name + ".pdf"
+        let meta = ImageAsset(imageName: pdf)
+        let jsonEncoder = JSONEncoder()
+        let data = try jsonEncoder.encode(meta)
+        try data.write(to: folder.appendingPathComponent("Contents.json"))
+
+        try manager.moveItem(at: URL(fileURLWithPath: file), to: folder.appendingPathComponent(pdf))
+    }
+}
--- Sources/AssetMate/Commands/Main.swift
+++ Sources/AssetMate/Commands/Main.swift
@@ -7,7 +7,7 @@ import ArgumentParser
 
 struct Main: ParsableCommand {
     static var configuration = CommandConfiguration(
-        abstract: "A CLI helper for working with HEX colors in iOS projects",
-        subcommands: [Identify.self, Generate.self, Inspect.self]
+        abstract: "A CLI helper for working with Asset.xcassets folder in iOS projects",
+        subcommands: [Identify.self, Generate.self, Inspect.self, Add.self]
     )
 }
