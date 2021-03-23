1a9b5ac1c9a2885ffc7c3aba85883ecd55f0a2d5
Alexey Korolev <alphatroya@gmail.com>
2021-03-23 14:49:02 +0300

    Add config-set command (#30)

--- Sources/AssetMate/Commands/Assetmate.swift
+++ Sources/AssetMate/Commands/Assetmate.swift
@@ -7,11 +7,12 @@ import ArgumentParser
 
 struct Assetmate: ParsableCommand {
     static var configuration = CommandConfiguration(
-        abstract: "A CLI helper for working with Asset.xcassets folder in iOS projects",
-        version: "0.0.31",
+        abstract: "A CLI helper for working with Assets.xcassets folder in iOS projects",
+        version: "0.0.32",
         subcommands: [
             AddColor.self,
             AddImage.self,
+            ConfigSet.self,
             ConvertAlpha.self,
             Group.self,
             Identify.self,
--- Sources/AssetMate/Helpers/AssetConfigLoader.swift
+++ Sources/AssetMate/Helpers/AssetConfigLoader.swift
@@ -7,32 +7,24 @@ import ArgumentParser
 import Foundation
 import Yams
 
-struct Configuration: Decodable {
+struct Configuration: Codable {
     var asset: Asset
 }
 
-protocol AssetConfigLoader {
+protocol AssetConfigLoader: ConfigParamsProvider {
     func load(_ asset: Asset?) throws -> Configuration
 }
 
 extension AssetConfigLoader {
-    private var decoder: YAMLDecoder {
-        YAMLDecoder()
-    }
-
-    private var fileName: String {
-        "assetmate.yml"
-    }
-
     private var configLoaderError: Error {
-        ValidationError("you should either set --asset option or specify it in config file (\(fileName))")
+        ValidationError("you should either set --asset option or specify it in config file (\(configFileName))")
     }
 
     func load(_ asset: Asset?) throws -> Configuration {
         if let asset = asset {
             return .init(asset: asset)
         }
-        let url = URL(fileURLWithPath: fileName)
+        let url = URL(fileURLWithPath: configFileName)
         guard let data = try? Data(contentsOf: url),
             let config = try? decoder.decode(Configuration.self, from: data) else {
             throw configLoaderError
