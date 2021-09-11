5adcde6944b9d6a0483ca08dfa421b6e23dcef7f
Alexey Korolev <alphatroya@gmail.com>
2021-09-11 11:00:21 +0300

    Update version to 0.0.34

--- Sources/AssetMate/Commands/Assetmate.swift
+++ Sources/AssetMate/Commands/Assetmate.swift
@@ -9,7 +9,7 @@ import ArgumentParser
 struct Assetmate: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A CLI helper for working with Assets.xcassets folder in iOS projects",
-        version: "0.0.33",
+        version: "0.0.34",
         subcommands: [
             AddColor.self,
             AddImage.self,
