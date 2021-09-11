229626141d04a29070b37708a55990b525413ba9
Alexey Korolev <alphatroya@gmail.com>
2021-09-11 11:03:36 +0300

    Update swift-argument-parser dependency to 1.0.0 (#32)

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .executable(name: "assetmate", targets: ["AssetMate"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/jpsim/Yams", .upToNextMinor(from: "4.0.0")),
         .package(url: "https://github.com/marmelroy/Zip", .upToNextMinor(from: "2.1.1")),
     ],
