8a33557c49c6a8418ab21e3665f7952d4006ac01
Alexey Korolev <alphatroya@gmail.com>
2025-02-20 09:39:49 +0300

    Update swift version in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,19 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "assetmate",
-    platforms: [.macOS(.v10_12)],
+    platforms: [.macOS(.v10_13)],
     products: [
         .executable(name: "assetmate", targets: ["AssetMate"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.0")),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.0")
+        ),
         .package(url: "https://github.com/jpsim/Yams", .upToNextMinor(from: "4.0.0")),
-        .package(url: "https://github.com/marmelroy/Zip", .branchItem("master")),
+        .package(url: "https://github.com/marmelroy/Zip", branch: "master"),
     ],
     targets: [
         .executableTarget(
